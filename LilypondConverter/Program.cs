3fcaf3dd2ab1f678362e6c1ad8be855f633990f4
József Putz <jozsef.putz@semilab.hu>
2020-01-31 15:24:27 +0100

    Xml loading.

--- LilypondConverter/Program.cs
+++ LilypondConverter/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace LilypondConverter
 {
@@ -10,7 +11,28 @@ namespace LilypondConverter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Converting");
+            Console.WriteLine("Converting MuseScore to Lilypond");
+
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(@".\Data\kotta.mscx");
+
+                //XmlNode node = doc.DocumentElement.SelectSingleNode("/book/title");
+                // XmlNode node = doc.DocumentElement.SelectSingleNode("/book/title");
+                //string attr = node.Attributes["theattributename"]?.InnerText
+                foreach (XmlNode node in doc.DocumentElement.ChildNodes)
+                {
+                    string text = node.InnerText; //or loop through its children as well
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);                
+            }
+
+
             Console.ReadKey();
         }
     }
