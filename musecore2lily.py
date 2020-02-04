af276301762260a2d6c980a2bfe8fe4afc8b3b95
putzjozsi <putzjozsi@gmail.com>
2020-02-04 20:12:28 +0100

    Rests, dots, ties

--- musecore2lily.py
+++ musecore2lily.py
@@ -10,49 +10,90 @@ def getPitchName(pitch, accidental):
   if(pitch == 0):
     if(accidental == SHARP):
       return 'his'
+    if(accidental == DOUBLE_FLAT):
+      return "deses"
     return 'c'
+
   if(pitch == 1):
     if(accidental == SHARP):
       return 'cis'
     if(accidental == FLAT):
       return 'des'
+    if(accidental == DOUBLE_SHARP):
+      return "hisis"
+
   if(pitch == 2):
+    if(accidental == DOUBLE_SHARP):
+      return 'cisis'
+    if(accidental == DOUBLE_FLAT):
+      return "eses"
     return 'd'
+
   if(pitch == 3):
     if(accidental == SHARP):
       return 'dis'
     if(accidental == FLAT):
       return 'es'
+    if(accidental == DOUBLE_FLAT):
+      return "feses"
+
   if(pitch == 4): 
     if (accidental == FLAT):
       return "fes"
+    if(accidental == DOUBLE_SHARP):
+      return 'disis'
     return 'e'
+
   if(pitch == 5):
     if(accidental == SHARP):
       return 'eis'
+    if(accidental == DOUBLE_FLAT):
+      return "geses"
     return 'f'
+
   if(pitch == 6):
     if(accidental == SHARP):
       return 'fis'
     if(accidental == FLAT):
       return 'ges'
+    if(accidental == DOUBLE_SHARP):
+      return 'eisis'
+
   if(pitch == 7):
+    if(accidental == DOUBLE_SHARP):
+      return "fisis"
+    if(accidental == DOUBLE_FLAT):
+      return "asas"
     return 'g'
+
   if(pitch == 8):
     if(accidental == SHARP):
       return 'gis'
     if(accidental == FLAT):
       return 'as'
+    if(accidental == DOUBLE_SHARP):
+      return "gisis"
+
   if(pitch == 9):
+    if(accidental == DOUBLE_SHARP):
+      return "gisis"
+    if(accidental == DOUBLE_FLAT):
+      return "heses"
     return 'a'
+
   if(pitch == 10):
     if(accidental == SHARP):
       return 'ais'
     if(accidental == FLAT):
-      return 'bes'
+      return 'b'
+    if(accidental == DOUBLE_FLAT):
+      return "ceses"
+   
   if(pitch == 11):
     if(accidental == FLAT):
       return "ces"
+    if(accidental == DOUBLE_SHARP):
+      return "aisis"
     return 'h'
 
 def getAccidentalSymbol(accidental):
@@ -70,7 +111,7 @@ def getAccidentalSymbol(accidental):
     
 def note2pitch(note, accidental):
   pitch = int(note) % 12
-  octave = int(int(note) / 12) # ezt hangnevbol kell kiszamolni
+  #octave = "'"
 
   accidentalSymbol = getAccidentalSymbol(accidental)
   pitchName = getPitchName(pitch, accidentalSymbol)
@@ -78,7 +119,7 @@ def note2pitch(note, accidental):
   if(accidentalSymbol is None):
     accidentalSymbol = ""
   
-  return "{0}{1}".format(pitchName, accidentalSymbol)
+  return "{0}".format(pitchName)
 
 def getNote(note):
   pitches = note.findall('pitch')
@@ -86,13 +127,14 @@ def getNote(note):
 
   pitch = 0
   accidental = ""
-  if( not (pitches is None) and len(pitches) > 0):
+  if(len(pitches) > 0):
     pitch = pitches[0].text
 
-  if( not (accidentals is None) and len(accidentals) > 0):
+  if(len(accidentals) > 0):
     accidental = accidentals[0].text
 
   pitchString = note2pitch(pitch, accidental)
+
   return pitchString
   
 
@@ -117,7 +159,31 @@ def getChord (chord):
   durationType = chord.findall('durationType')
   duration = getDuration(durationType[0].text)
 
+  # Dot
+  dotsType = chord.findall('dots')
+  if(len(dotsType) > 0):
+    duration += '.'
+
+  # Tie
+  tie = chord.findall('Note/Spanner/Tie')
+  if(len(tie) > 0):
+    duration += '~'
+
   result += str(duration)
+  return result
+
+def getRest (rest):  
+  durationType = rest.findall('durationType')
+  duration = getDuration(durationType[0].text)
+
+  if(duration is None):
+    return ''
+
+  dotsType = rest.findall('dots')
+  if(len(dotsType) > 0):
+    duration += '.'
+
+  result = " r" + str(duration)
   return result
 
 def convert (file) :
@@ -125,9 +191,11 @@ def convert (file) :
   print(root)
   lilypondCode = ""
   for staff in root.findall('Score/Staff'):
+      staffId = staff.get('id')
       print("===============")    
-      print("Staff " + staff.get('id'))
+      print("Staff " + staffId)
       print("===============")
+      lilypondCode += "staff" + staffId + " = {"
       measureNumber=1
       for measure in staff.findall('Measure'):
        # print(str(measureNumber))
@@ -136,12 +204,17 @@ def convert (file) :
           for child in list(voice):
             if(child.tag == "Chord"):
               chord = getChord(child)
-              lilypondCode += chord + " "
-           # if(child.tag == "Rest"):
-             # print(child.tag)             
+              lilypondCode += chord
+            if(child.tag == "Rest"):
+              rest = getRest(child)
+              lilypondCode += rest
+           
+      lilypondCode += " }"             
   print(lilypondCode)
 
-convert('accidentals.mscx')
+#convert('accidentals.mscx')
+convert('szunetek-pontzott.mscx')
+
  
 
 
